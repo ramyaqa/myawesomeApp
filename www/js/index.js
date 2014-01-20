f54b2865af1828f2a3c8f333432082d80451f8ed
Arvind Palaniswamy <parvind_15@hotmail.com>
2014-01-19 22:11:42 -0800

    ondeviceready

--- www/js/index.js
+++ www/js/index.js
@@ -20,7 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        console.log('binding');
+        console.log('bound events');
         
     },
     // Bind Event Listeners
