43b695ad0e92e5569a6b29be0db2ae22a7a2533e
Arvind Palaniswamy <parvind_15@hotmail.com>
2014-01-19 22:21:25 -0800

    removed plugin

--- platforms/android/.staging/www/cordova_plugins.js
+++ platforms/android/.staging/www/cordova_plugins.js
@@ -1,24 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
-        "id": "org.apache.cordova.dialogs.notification",
-        "merges": [
-            "navigator.notification"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
-        "id": "org.apache.cordova.dialogs.notification_android",
-        "merges": [
-            "navigator.notification"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "org.apache.cordova.dialogs": "0.2.5"
-}
+{}
 // BOTTOM OF METADATA
 });
